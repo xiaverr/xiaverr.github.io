06e5ef17f4cfda391be7b41c1d6f320ca98237e8
xiaverr <2723150907@qq.com>
2026-04-16 09:57:37 +0800

    Site updated: 2026-04-16 09:57:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["My-First-Hexo-Blog/","【CUDA-C】CUDA编程模型/"];function toRandomPost(){
+var posts=["【CUDA-C】CUDA编程模型/","我的第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
