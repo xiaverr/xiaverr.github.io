6920256f40087bc5af113ad4397bce35209fd026
xiaverr <2723150907@qq.com>
2026-04-19 22:45:00 +0800

    Site updated: 2026-04-19 22:45:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["【CUDA-C】CUDA编程模型/","我的第一篇博客/","【CUDA-C】CUDA编程基础/"];function toRandomPost(){
+var posts=["【CUDA-C】CUDA编程基础/","【CUDA-C】CUDA编程模型/","【mooncake】mooncake架构分析和代码解读/","我的第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
