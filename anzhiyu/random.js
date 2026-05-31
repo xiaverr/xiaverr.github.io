5cfa50ec71746aa95199db17727128ab06af9b74
xiaverr <2723150907@qq.com>
2026-05-31 19:50:13 +0800

    Site updated: 2026-05-31 19:50:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["【CUDA-C】CUDA编程基础/","【CUDA-C】CUDA编程模型/","【cpp】多线程编程/","【mooncake】mooncake架构分析和代码解读/","我的第一篇博客/","【infra】vllm-lmcache-mooncake部署/"];function toRandomPost(){
+var posts=["【CUDA-C】CUDA编程基础/","【CUDA-C】CUDA编程模型/","【cpp】多线程编程/","【mooncake】mooncake架构分析和代码解读/","我的第一篇博客/","【infra】vllm-lmcache-mooncake部署/","【Mooncake】Mooncake的使用方法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
